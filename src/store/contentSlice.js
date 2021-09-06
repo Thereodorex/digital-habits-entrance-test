78d7cb59eea62f09f416d7aabc667a2992d4b1d3
Jamil Corwin <kajdhfglksdjhfglkhj@gmail.com>
2021-09-07 00:59:56 +0300

    Readme

--- src/store/contentSlice.js
+++ src/store/contentSlice.js
@@ -23,6 +23,7 @@ const postsSlice = createSlice({
   initialState: {},
   reducers: {
     removeFile(state, action) {
+      delete state[action.payload.id];
     },
   },
   extraReducers: (builder) => {
